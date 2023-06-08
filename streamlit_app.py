9797db4057836d5ee0917da7549ab7d9f6d42b4c
Shalini-bs <135714974+Shalini-bs@users.noreply.github.com>
2023-06-08 16:23:33 +0530

    Update streamlit_app.py
    Changee fruit name in url

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("http://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #just writes the data to the screen
+fruityvice_response = requests.get("http://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 
 #json file to dataframe like table normalizing
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
