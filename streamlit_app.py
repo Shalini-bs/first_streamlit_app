314d0f308245245aa211aa16687bb03b6891c3e2
Shalini-bs <135714974+Shalini-bs@users.noreply.github.com>
2023-06-09 22:09:34 +0530

    Update streamlit_app.py
    Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ def get_fruit_load_list():
 #Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list() 
-    streamlit.dataframe(my_data_rows) o
+    streamlit.dataframe(my_data_rows)
 
 #allow end user to add a fruit to list
 def insert_row_snowflake(new_fruit):
